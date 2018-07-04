a3ed6f2bfadb7b3dc990226d60e8467ac5ba4602
Jet <397790001@qq.com>
2018-07-04 19:38:27 +0800

    Initial commit

---
+++ exercise_everyday/20180704.cpp
@@ -0,0 +1,27 @@
+#include "stdafx.h"
+#include "20180704.h"
+
+void Solution::test()
+{
+	cout << "test 1:" << solution(1).c_str() << endl;
+	cout << "test 1:" << solution(218).c_str() << endl;
+}
+
+string Solution::solution(int input)
+{
+	if (input <= 0) { return "false"; }
+	int end = input / 2;
+	int cur = 1;
+	for (int i = 0; i <= end; i++)
+	{
+		if (cur > input)
+		{
+			return "false";
+		}
+		else if (cur == input)
+		{
+			return "true";
+		}
+		cur *= 2;
+	}
+}
---
+++ exercise_everyday/20180704.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+class Solution
+{
+public:
+	void test();
+	string solution(int);
+};
