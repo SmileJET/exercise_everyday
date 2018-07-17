738c65f2495fc5989981051916d0ac0019955be6
Jet <397790001@qq.com>
2018-07-17 20:26:15 +0800

    answer 20180716

--- 20180716/20180716.h
+++ 20180716/20180716.h
@@ -21,6 +21,7 @@ namespace day_20180716
 			cout << input.c_str() << endl;
 			string res = solution(dict, input);
 			cout << "result:" << res << endl;
+			answer(dict, input);
 		}
 
 		string solution(vector<string> dict, string input)
@@ -61,11 +62,37 @@ namespace day_20180716
 			return word;
 		}
 
-		void answer()
+		void answer(vector<string>& dict, string sentence)
 		{
-		
+			string res = replaceWords(dict, sentence);
+			cout << "answer:" << endl << res << endl;
 		}
 
+		string replaceWords(vector<string>& dict, string sentence)
+		{
+			string res = "", t = "";
+			vector<vector<string>> v(26);
+			istringstream is(sentence);
+			sort(dict.begin(), dict.end(), [](string &a, string &b) {return a.size() < b.size(); });
+			for (string word : dict)
+			{
+				v[word[0] - 'a'].push_back(word);
+			}
+			while (is >> t)
+			{
+				for (string word : v[t[0] - 'a'])
+				{
+					if (t.substr(0, word.size()) == word)
+					{
+						t = word;
+						break;
+					}
+				}
+				res += t + " ";
+			}
+			res.pop_back();
+			return res;
+		}
 	};
 
 }
