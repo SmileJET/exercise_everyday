0733bc3836d8403664f51bbf2da892812823bf6a
Jet <397790001@qq.com>
2018-07-19 09:03:39 +0800

    answer 20180717

--- 20180717/20180717.h
+++ 20180717/20180717.h
@@ -21,6 +21,8 @@ namespace day_20180717
 			cout << "input:" << input << "\toutput:" << solution(input) << endl;
 			input = 1;
 			cout << "input:" << input << "\toutput:" << solution(input) << endl;
+
+			answer();
 		}
 
 		bool solution(int num)
@@ -40,7 +42,6 @@ namespace day_20180717
 				{
 					new_num = new_num * 10 + list[i];
 				}
-				cout << "new num:" << new_num << endl;
 				if (is_power_of_2(new_num)) { return true; }
 			} while (next_permutation(list.begin(), list.end()));
 			
@@ -55,10 +56,36 @@ namespace day_20180717
 
 		void answer()
 		{
-			
+			cout << "answer:" << endl;
+			int input = 16, output;
+			cout << "input:" << input << "\toutput:" << recorderedPowerOf2(input) << endl;
+			input = 10;
+			cout << "input:" << input << "\toutput:" << recorderedPowerOf2(input) << endl;
+			input = 1;
+			cout << "input:" << input << "\toutput:" << recorderedPowerOf2(input) << endl;
 		}
 
-		
+		bool recorderedPowerOf2(int n)
+		{
+			int i, j, k, tmp;
+			vector<int> a;
+			a.clear();
+			while (n > 0)
+			{
+				a.push_back(n % 10);
+				n /= 10;
+			}
+			sort(a.begin(), a.end());
+			do
+			{
+				if (a[0] == 0)continue;
+				tmp = 0;
+				for (i = 0; i < a.size(); i++)
+					tmp = tmp * 10 + a[i];
+				if ((tmp&tmp - 1) == 0)return true;
+			} while (next_permutation(a.begin(), a.end()));
+			return false;
+		}
 	};
 
 }
