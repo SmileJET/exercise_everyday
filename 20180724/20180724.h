8669b33697968227a3737bcb80ceca0fbcda15ec
Jet <397790001@qq.com>
2018-07-25 13:17:05 +0800

    answer 20180724

--- 20180724/20180724.h
+++ 20180724/20180724.h
@@ -5,6 +5,7 @@
 #include <vector>
 #include <algorithm>
 #include <sstream>
+#include <map>
 
 using namespace std;
 
@@ -19,6 +20,7 @@ namespace day_20180724
 			string input2 = "fviefuro";
 			cout << "input:" << input1.c_str() << "\toutput:" << solution(input1).c_str() << endl;
 			cout << "input:" << input2.c_str() << "\toutput:" << solution(input2).c_str() << endl;
+			answer();
 		}
 
 		string solution(string input)
@@ -51,8 +53,40 @@ namespace day_20180724
 
 		void answer()
 		{
-
+			cout << "answer:" << endl;
+			string input1 = "owoztneoer";
+			string input2 = "fviefuro";
+			cout << "input:" << input1.c_str() << "\toutput:" << originalDigits(input1).c_str() << endl;
+			cout << "input:" << input2.c_str() << "\toutput:" << originalDigits(input2).c_str() << endl;
 		}
+
+		string originalDigits(string s) {
+			map<char, int> myMap{
+				{'g', 8}, {'x',6},{'u',4},{'w',2},{'z',0},
+			{'f',5},{'h',3},{'s',7},{'o',1},{'i',9}
+			};
+			vector<int> numbers(10, 0);
+			string res;
+
+			for (auto p : myMap) {
+				numbers[p.second] = count(s.begin(), s.end(), p.first);
+			}
+
+			numbers[5] -= numbers[4];
+			numbers[3] -= numbers[8];
+			numbers[7] -= numbers[6];
+			numbers[1] -= (numbers[0] + numbers[2] + numbers[4]);
+			numbers[9] -= (numbers[5] + numbers[6] + numbers[8]);
+
+			for (int i = 0; i < 10; i++) {
+				while (numbers[i] > 0) {
+					res += ('0' + i);
+					numbers[i]--;
+				}
+			}
+
+			return res;
+		} 
 	};
 
 }
